e1a87a196840708f968dd1fdbc717b682bf4822a
premkiran7 <premkiran7@gmail.com>
2020-10-09 18:37:28 +0530

    [version-1] created a basic version of my portfolio

---
+++ scripts.js
@@ -0,0 +1,12 @@
+VANTA.NET({
+  el: "#welcome-section",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x1ab122,
+  backgroundColor: 0xf7f8f3,
+});
